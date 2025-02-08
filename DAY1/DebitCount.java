b12121b3f192ec2b5848e9bd2f82496fab46c79b
VaishnaviWagle <vaishnaviwagle936@gmail.com>
2025-02-08 09:19:10 +0530

    Update DebitCount.java

--- DAY1/DebitCount.java
+++ DAY1/DebitCount.java
@@ -1,35 +1,3 @@
-mport java.util.Scanner;
-
-public class DebitCount {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Taking input for number of transactions
-        System.out.print("Enter the number of transactions: ");
-        int n = scanner.nextInt();
-
-        int[] transactions = new int[n];
-
-        // Taking input for transactions
-        System.out.println("Enter the transaction amounts:");
-        for (int i = 0; i < n; i++) {
-            transactions[i] = scanner.nextInt();
-        }
-
-        // Counting debits (when current transaction > next transaction)
-        int debitCount = 0;
-        for (int i = 0; i < n - 1; i++) {
-            if (transactions[i] > transactions[i + 1]) {
-                debitCount++;
-            }
-        }
-
-        // Printing the number of debits
-        System.out.println("Number of debit transactions: " + debitCount);
-
-        scanner.close();
-    }
-}
 
     
 
